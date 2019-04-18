5429c54e8fb10df9da7991ec5e77d2ad7ee9d255
MbusoMakitla <1699140@students.wits.ac.za>
2019-04-18 17:33:24 +0200

    Rendering function

--- world.js
+++ world.js
@@ -29,3 +29,25 @@ var axishelper = {},
     skyGeometry = {},
     skyMaterial = {},
     skyTexture = {};
+
+//This function is for the sole purpose of rendering
+function render () {
+    globalRenderID = requestAnimationFrame( render ); //How are we rendering globally
+    controls.update(); // We are updating the controls as we render different parts
+
+    powerups.forEach( function ( element, index ) {
+        powerups[ index ].animate(); // This is our life span, only one in the case
+    });
+
+    mountains.forEach( function ( element, index ) { //We are rendering the mountains as our world and scene progresses
+        mountains[ index ].animate();
+    });
+
+    if ( detectCollisions( powerups ) === true ) { //if we touch an obstacle, it's game over
+        gameOver();
+    }
+
+    renderer.render( scene, camera ); // I dont get this line yet
+}
+
+//The next function is dealing with how we declare if the game is over or not
