a982612e9764895233d13d7a1e51bc905d6404ce
PermanentPortionX <1627982@students.wits.ac.za>
2019-04-26 23:15:17 +0200

    added more and more comments

--- js/GameLogicBuilder.js
+++ js/GameLogicBuilder.js
@@ -11,6 +11,27 @@
 /******************************************************************************/
 
 
+
+
+//this list contains specifications of every level
+//the first list [1] is an indicator of which level the game is at
+//the list with 6 values, represents 1 block of the scene
+//a block has width 5 and height 6, each value in the list represents
+//a row of the block, for simplicity look at the block as a 6 x 5 matrix
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+//from gameBuildList, each list has 6 values which describe how the row must be rendered
+//e.g. add obstacles, add power ups
+//the children(inner list) of gameBuildList contains values that index another list called
+//block set //-- PARENT: BLOCKSET.js --//, this list is the one that has possible combinations of
+//obstacles that can be added onto a scene, for simplicity look at this whole thing in terms of
+//INDEXED FACE SETS
+
+//Obstacles symbols
 // 1 -> Spikes
 // 2 -> Cube
 const gameBuildList = [
@@ -26,14 +47,15 @@ const gameBuildList = [
 function buildGame() {
 
     lastPos += 6;
+    //iterate through each list in gameBuildList and build a game block that represents it
     for (let i = 0; i < gameBuildList.length; i++) {
         let blockSection = gameBuildList[i];
 
         if (blockSection.length === 1) {
-            //level indicator
+            //notify user of the new level they at
+            //configure game behavior
         }
         else{
-
             const blockScene = new THREE.Scene( );
             //add defaults
             blockScene.add(ground.clone());
@@ -61,7 +83,6 @@ function buildGame() {
                             else{
                                 if (k === 4) xPos = -1.8;
                                 else xPos = -0.9;
-
                             }
 
                             //calculate the position of z
